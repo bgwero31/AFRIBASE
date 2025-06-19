81920347edeab0351c156d6901d64578f6639ac3
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-06-19 16:20:54 +0200

    Create Market.js

---
+++ src/pages/Market.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Market = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>🛍️ Marketplace</h2>
+      <p>Post or browse items for sale</p>
+    </div>
+  );
+};
+
+export default Market;
