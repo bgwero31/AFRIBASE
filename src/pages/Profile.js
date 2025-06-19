2999d82cea5bb72611540cb93ca81a15c5b6afce
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-06-19 16:20:14 +0200

    Create Profile.js

---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>👤 My Profile</h2>
+      <p>Update info, view stats, VIP access</p>
+    </div>
+  );
+};
+
+export default Profile;
