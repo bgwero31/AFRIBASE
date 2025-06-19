ce84f563dce526821ca4b20f624ef13c0e968f08
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-06-19 19:33:42 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDW5ntQiR9cF-qZ9vOGY49P6hTOUhTPxdc",
+  authDomain: "neurobet-ai.firebaseapp.com",
+  projectId: "neurobet-ai",
+  storageBucket: "neurobet-ai.firebasestorage.app",
+  messagingSenderId: "793175611155",
+  appId: "1:793175611155:web:10d91cb8d4fca30f6eece6",
+  measurementId: "G-RPZZ9MPQTJ",
+  databaseURL: "https://neurobet-ai-default-rtdb.firebaseio.com" // ✅ Add this line manually
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
