2b221085ea0268635185b7e6075a4fe167cd6735
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-06-19 16:19:27 +0200

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>🔥 Welcome to Afribase</h1>
+      <p>Connect • Chat • Hustle • Sell</p>
+    </div>
+  );
+};
+
+export default Home;
