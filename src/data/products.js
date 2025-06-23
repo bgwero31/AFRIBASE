a5f07533eb524dcc4ff624d37f007f65450785cc
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-06-23 15:34:16 +0200

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -5,14 +5,18 @@ const products = [
     title: "EcoCash T-shirt",
     description: "Stylish and comfy. Hustler wear.",
     price: "10 USD",
-    image: "https://i.imgur.com/W4hTndS.png"
+    image: "https://i.imgur.com/W4hTndS.png",
+    ownerUID: "user123",
+    ownerName: "Tino"
   },
   {
     id: 2,
     title: "Solar Powerbank",
     description: "Charge anywhere, no zesa problems.",
     price: "25 USD",
-    image: "https://i.imgur.com/7yUvePI.jpeg"
+    image: "https://i.imgur.com/7yUvePI.jpeg",
+    ownerUID: "user456",
+    ownerName: "Mai Energy"
   }
 ];
 
